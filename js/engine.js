666bb7a57a73be55a11fefba55019bbeb8fd530c
Martina Fernandez <martinalucia.fdez@gmail.com>
2018-11-20 10:49:37 +0000

    restrucuring code to see if something else works

--- js/engine.js
+++ js/engine.js
@@ -16,6 +16,55 @@ $('button').click(function(event){
   compare(play_you, play_machine);
 });
 
+/*Randomise value for what the computer would do */
+/*function for the computers move*/
+function computer_move(){
+  var play_machine = Math.floor(Math.random() * (3 - 0)) + 0; /*want to go over maximum as its excluded but minimum is included*/
+  play_machine = choices[play_machine];
+  $("#play_machine").text(play_machine);
+  console.log('move: ' + play_machine);
+  return play_machine;
+}
+
+/*function for comparing you and computer*/
+function compare(you, machine){
+  var verdict = 'lose';
+  if ( you == machine ) {
+    verdict = 'draw';
+  }
+  if ((you=="rock") && (machine == "scissors")){
+    verdict='win';
+  }
+  if ((you=="paper") && (machine == "rock")){
+    verdict = 'win';
+  }
+  if ((you=="scissors") && (machine == "paper")){
+    verdict = 'win';
+  }
+  $("#verdict").text(verdict);
+  played++;
+  $("#game_played").text(played)
+
+function score() {
+  switch(verdict){
+  case 'win':
+  won++;
+  $("#game_won").text(won);
+  break;
+
+  case 'lose':
+  lost++;
+  $("#game_lost").text(lost);
+  break;
+
+  case 'draw':
+  draw++;
+  $("#game_draw").text(draw);
+  break;
+}
+}
+
+
 /*$("#rock").click(function(event){
 var play_you = 'rock';
 $("#play_you").text(play_you)
@@ -34,68 +83,3 @@ $("#play_you").text(play_you)
 var play_machine= computer_move();
 compare(play_you, play_machine);
 });*/
-
-/*Randomise value for what the computer would do */
-/*function for the computers move*/
-function computer_move(){
-  var play_machine = Math.floor(Math.random() * (3 - 0)) + 0; /*want to go over maximum as its excluded but minimum is included*/
-  play_machine = choices[play_machine];
-  $("#play_machine").text(play_machine);
-  console.log('move: ' + play_machine);
-  return play_machine;
-}
-
-/*function for comparing you and computer*/
-
-function compare(you, machine){
-  var verdict = 'lose';
-  if ( you == machine ) {
-    verdict = 'draw';
-  }
-  if ((you=="rock") && (machine == "scissors")){
-    verdict='win';
-  }
-  if ((you=="paper") && (machine == "rock")){
-    verdict = 'win';
-  }
-  if ((you=="scissors") && (machine == "paper")){
-    verdict = 'win';
-  }
-  if (verdict == 'win') {
-    won++;
-    $("#game_won").text(won);
-  }
-  if (verdict == 'lose') {
-    lost++;
-    $("#game_lost").text(lost);
-  }
-  if (verdict == 'draw') {
-    draw++;
-    $("#game_draw").text(draw);
-  }
-  $("#verdict").text(verdict);
-  played++;
-  $("#game_played").text(played)
-}
-
-/*score function*/
-
-function score() {
-
-  switch(verdict){
-  case 'win':
-  won++;
-  $("#won").text(won);
-  break;
-
-  case 'lose':
-  lost++;
-  $("#lost").text(lost);
-  break;
-
-  case 'draw':
-  draw++;
-  $("#draw").text(draw);
-  break;
-}
-}
