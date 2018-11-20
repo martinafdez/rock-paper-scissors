5cff6dca7da88c04bbc833ba0c3dc2705663d23d
Martina Fernandez <martinalucia.fdez@gmail.com>
2018-11-20 10:32:04 +0000

    making button only into one click and trying to break up code into three functions

--- js/engine.js
+++ js/engine.js
@@ -6,52 +6,50 @@ var won = 0;
 var lost = 0;
 var draw = 0;
 
-
-
 /*Listen for the button clicks the hashtag calls the button in html, then it is called via id and stored in a variable. */
-$("#rock").click(function(event){
+
+$('button').click(function(event){
+  var play_you = this.getAttribute("value");
+  console.log(play_you);
+  $("#play_you").text(play_you)
+  var play_machine= computer_move();
+  compare(play_you, play_machine);
+});
+
+/*$("#rock").click(function(event){
 var play_you = 'rock';
 $("#play_you").text(play_you)
 var play_machine= computer_move();
 compare(play_you, play_machine);
 });
-
-
 $("#paper").click(function(event){
 var play_you = 'paper';
 $("#play_you").text(play_you)
 var play_machine= computer_move();
 compare(play_you, play_machine);
 });
-
-
 $("#scissors").click(function(event){
 var play_you = 'scissors';
 $("#play_you").text(play_you)
 var play_machine= computer_move();
 compare(play_you, play_machine);
-});
+});*/
 
 /*Randomise value for what the computer would do */
-
-
-
+/*function for the computers move*/
 function computer_move(){
   var play_machine = Math.floor(Math.random() * (3 - 0)) + 0; /*want to go over maximum as its excluded but minimum is included*/
-
   play_machine = choices[play_machine];
   $("#play_machine").text(play_machine);
   console.log('move: ' + play_machine);
-
-
   return play_machine;
 }
+
+/*function for comparing you and computer*/
 
 function compare(you, machine){
 
   var verdict = 'lose';
-
-
   if ( you == machine ) {
     verdict = 'draw';
   }
@@ -83,10 +81,26 @@ function compare(you, machine){
     $("#game_draw").text(draw);
   }
 
-   $("#verdict").text(verdict);
 
-   played++;
-   $("#game_played").text(played);
+  $("#verdict").text(verdict);
+  played++;
+  $("#game_played").text(played)
+}
 
 
+function score(switch(verdict)) {
+  case 'win':
+  won++;
+  $("#won").text(won);
+  break;
+
+  case 'lose':
+  lost++;
+  $("#lost").text(lost);
+  break;
+
+  case 'draw':
+  draw++;
+  $("#draw").text(draw);
+  break;
 }
